9bdb5f128bc3a40222d8221b05d121e17a1b390c
Tanvi1369 <88534668+Tanvi1369@users.noreply.github.com>
2021-08-09 21:11:02 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+var btn_red;
+var btn_green;
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+  
+
+}
+
+function draw() {
+ background(r,g,b);
+ 
+ if (keyIsDown(RIGHT_ARROW))
+ {
+    background("RED");
+ }
+if (keyIsDown(LEFT_ARROW))
+{
+ background("GREEN");
+}
+if (keyIsDown(UP_ARROW))
+{
+ background("PINK");
+}
+if (keyIsDown(DOWN_ARROW))
+{
+ background("YELLOW");
+}
+
+ drawSprites();
+}
+
